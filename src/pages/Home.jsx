995a7d19ed675c5045b646772291661d8d8a82da
leslee <lesleenyanducha@gmail.com>
2026-08-12 20:50:59 +0300

    fix: add missing Link import to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Fragment, useEffect, useMemo, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import SiteChrome from '../components/SiteChrome.jsx'
